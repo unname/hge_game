2f329e62991140ca315b98138032bf5e6478f3c5
Anikushin <Anikushin@rutoken.ru>
2015-08-06 13:10:49 +0300

    Added xml_parser / Create c_loadmap class

---
+++ hge_game/c_loadmap.cpp
@@ -0,0 +1,26 @@
+#include "c_loadmap.h"
+
+
+c_loadmap::c_loadmap()
+{
+    XMLDocument TME_MAP;
+    TME_MAP.LoadFile("resources/test_tmx.tmx");
+
+    XMLElement *Map = NULL;
+    Map = TME_MAP.FirstChildElement("map");
+
+    // Ўирина и высота карты в тайлах
+    size_t MapWidth, MapHeight;
+    size_t TileWidth, TileHeight;
+
+    MapWidth = atoi(Map->Attribute("width"));
+    MapHeight = atoi(Map->Attribute("height"));
+
+    TileWidth = atoi(Map->Attribute("tilewidth"));
+    TileHeight = atoi(Map->Attribute("tileheight"));
+}
+
+c_loadmap::~c_loadmap()
+{
+
+}
---
+++ hge_game/c_loadmap.h
@@ -0,0 +1,20 @@
+#ifndef C_LOADMAP_H
+#define C_LOADMAP_H
+#pragma once
+
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+
+class c_loadmap
+{
+public:
+    c_loadmap();
+    ~c_loadmap();
+
+private:
+
+    
+};
+
+#endif
